80b639b23de1fef3f1e1eb8bb42d0ea4fa7a5b43
Iván Landaverde <70768723+IvanMendoz@users.noreply.github.com>
2023-02-22 21:52:07 -0600

    Parcial finalizado

--- app/src/main/java/com/example/parcial1ivanmendozaetsp1/MainActivity.java
+++ app/src/main/java/com/example/parcial1ivanmendozaetsp1/MainActivity.java
@@ -2,20 +2,66 @@ package com.example.parcial1ivanmendozaetsp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edUsuario, edPass;
+    EditText edUser, edPass;
+    Button btAlerta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edUsuario = findViewById( R.layout.edtUsuario );
-        edPass = findViewById( R.layout.edtPass );
+        edUser = findViewById( R.id.edtUsuario );
+        edPass = findViewById( R.id.edtPass );
+        btAlerta = findViewById( R.id.btnAlerta );
+
+    }
+
+    public void login( View view){
+        String usuario = edUser.getText().toString(),
+                contra = edPass.getText().toString();
+
+        if ( usuario.equals("parcialETps1") && contra.equals("p4rC14l#tp$") ){
+            btAlerta.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intento =new Intent( getApplicationContext(), Principal.class );
+                    startActivity( intento );
+                }
+            });
+        }else{
+
+            btAlerta.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast toastp = new Toast( getApplicationContext());
+                    LayoutInflater inflater = getLayoutInflater();
+                    View layout =inflater.inflate( R.layout.toast_layout, ( ViewGroup ) findViewById(R.id.lyMensaje) );
+
+                    TextView txtMensaje = ( TextView ) layout.findViewById( R.id.txvAlerta );
+                    toastp.setDuration( Toast.LENGTH_LONG );
+                    toastp.setView( layout );
+                    toastp.show();
+                }
+            });
+        }
+    }
+
+    public void pantalla( View view ) {
+        Intent intento = new Intent( this, Principal.class);
+        startActivity( intento );
+
     }
 }
--- app/src/main/java/com/example/parcial1ivanmendozaetsp1/Principal.java
+++ app/src/main/java/com/example/parcial1ivanmendozaetsp1/Principal.java
@@ -3,17 +3,64 @@ package com.example.parcial1ivanmendozaetsp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class Principal extends AppCompatActivity {
 
-    EditText ejemplo;
+    EditText edPeso, edEstatura;
+    TextView mostrarMensaje, tvIMC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        ejemplo = findViewById( R.layout.)
+        edPeso = findViewById( R.id.edtPeso );
+        edEstatura = findViewById( R.id.edtEstatura );
+        mostrarMensaje = findViewById( R.id.txvResultado );
+        tvIMC = findViewById( R.id.txvValorIMC );
+    }
+
+    public void Calcular( View view ){
+        double peso = Double.parseDouble( edPeso.getText().toString() );
+        double estatura = Double.parseDouble( edEstatura.getText().toString() );
+        double IMC = 0.00;
+        String alerta = "";
+
+        IMC = peso / ( estatura * estatura );
+        //<>
+        if ( IMC < 10.5 ){
+            alerta = "Criticamente bajo de peso";
+        } else if ( IMC < 15.9 ) {
+            alerta = "Severamente bajo de peso";
+        } else if ( IMC < 18.5 ) {
+            alerta = "Bajo de peso";
+        } else if ( IMC < 25 ) {
+            alerta = "Normal (peso saludable)";
+        } else if ( IMC < 30 ) {
+            alerta = "Sobrepeso";
+        } else if ( IMC < 35 ) {
+            alerta = "Obesidad Clase 1 - Moderadamente obeso";
+        } else if ( IMC < 40 ) {
+            alerta = "Obesidad Clase 2 - Severamente obeso";
+        } else if ( IMC > 50 ) {
+            alerta = "Obesidad Clase 3 - Criticamente Obeso";
+        }
+
+
+        mostrarMensaje.setText("");
+        tvIMC.setText("");
+        tvIMC.setText( String.valueOf( obtieneDosDecimales( IMC ) ) );
+        mostrarMensaje.setText( alerta );
+    }
+
+    private String obtieneDosDecimales(double valor){
+        DecimalFormat format = new DecimalFormat();
+        format.setMaximumFractionDigits(2); //Define 2 decimales.
+        return format.format(valor);
     }
 }
